5861a954f22b594197d04fa46768c9e7592f36db
jiwoo <sgo8308@gmail.com>
2022-11-09 16:38:31 +0900

    init

---
+++ DB/ConcurrencyProblem/src/main/java/com/db/concurrencyproblem/ConcurrencyProblemApplication.java
@@ -0,0 +1,13 @@
+package com.db.concurrencyproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyProblemApplication.class, args);
+	}
+
+}
---
+++ DB/ConcurrencyProblem/src/test/java/com/db/concurrencyproblem/ConcurrencyProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.concurrencyproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
